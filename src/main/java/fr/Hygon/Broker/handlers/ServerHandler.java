ee9045ffb7b272705dced018f1b40f97adc763a1
Hygon <hygon806@gmail.com>
2021-10-25 22:42:15 +0200

    Packets were still being merged...

--- src/main/java/fr/Hygon/Broker/handlers/ServerHandler.java
+++ src/main/java/fr/Hygon/Broker/handlers/ServerHandler.java
@@ -9,28 +9,31 @@ import fr.Hygon.Broker.packets.Packets;
 import java.util.UUID;
 
 public class ServerHandler extends ChannelInboundHandlerAdapter {
-    private ByteBuf packetBuf;
+    private ByteBuf byteBuffer;
 
     @Override
     public void handlerAdded(ChannelHandlerContext ctx) {
-        packetBuf = ctx.alloc().buffer(4);
+        byteBuffer = ctx.alloc().buffer(4);
     }
 
     @Override
     public void handlerRemoved(ChannelHandlerContext ctx) {
-        packetBuf.release();
-        packetBuf = null;
+        byteBuffer.release();
+        byteBuffer = null;
     }
 
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object message) {
         ByteBuf inBuffer = (ByteBuf) message;
-        packetBuf.writeBytes(inBuffer);
+        byteBuffer.writeBytes(inBuffer);
         inBuffer.release();
 
-        if(packetBuf.readableBytes() >= 4) {
-            int packetSize = packetBuf.readInt();
-            if(packetBuf.readableBytes() >= packetSize) {
+        if(byteBuffer.readableBytes() >= 4) {
+            int packetSize = byteBuffer.readInt();
+            if(byteBuffer.readableBytes() >= packetSize) {
+                ByteBuf packetBuf = ctx.alloc().buffer(packetSize);
+                byteBuffer.readBytes(packetBuf);
+
                 int packetID = packetBuf.readInt();
                 Packet packet = Packets.getPacketByID(packetID);
                 if(packet == null) {
@@ -42,10 +45,9 @@ public class ServerHandler extends ChannelInboundHandlerAdapter {
                 long leastSignificantBits = packetBuf.readLong();
 
                 packet.read(ctx, new UUID(mostSignificantBits, leastSignificantBits), packetBuf);
-                packetBuf = ctx.alloc().buffer(4);
             } else {
-                packetBuf.resetReaderIndex();
-                packetBuf.resetWriterIndex();
+                byteBuffer.resetReaderIndex();
+                byteBuffer.resetWriterIndex();
             }
         }
     }
